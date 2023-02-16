f7d8a7995a76937bf35f8b3d21ef913a8885a8cd
Shaikh Afroz <shaikhafroz@geekyants.com>
2023-02-16 10:53:17 +0530

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView
+      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+export default App;
